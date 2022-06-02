d0b9d375291acf0bcafaeeec93debe749e97e133
Cute Inspire <Randywahidin321@gmail.com>
2022-06-03 04:45:26 +0700

    Update Soundcloud.py

--- KillerXMusic/platforms/Soundcloud.py
+++ KillerXMusic/platforms/Soundcloud.py
@@ -12,7 +12,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from KillerXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
