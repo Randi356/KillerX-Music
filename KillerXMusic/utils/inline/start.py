12afc4a0d5939bf9c7bb4268908f86466390c543
Cute Inspire <Randywahidin321@gmail.com>
2022-06-05 00:17:17 +0700

    fixed

--- KillerXMusic/utils/inline/start.py
+++ KillerXMusic/utils/inline/start.py
@@ -1,34 +1,21 @@
-#
-# Copyright (C) 2021-2022 by TeamKillerX@Github, < https://github.com/TeamKillerX >.
-#
-# This file is part of < https://github.com/TeamKillerX/KillerXMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamKillerX/KillerXMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
 
- from typing import Union
+from typing import Union
 
- from pyrogram.types import InlineKeyboardButton
- from KillerXMusic import app
+from pyrogram.types import InlineKeyboardButton
 
- from config import (
-    GITHUB_REPO, 
-    SUPPORT_CHANNEL, 
-    SUPPORT_GROUP, 
-    START_IMG_URL
-) 
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from KillerXMusic import app
 
 
 def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_9"],
-                url=f"https://t.me/t.me/VegetaSupports",
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?start=help",
             ),
             InlineKeyboardButton(
-                text=_["S_B_10"], url=f"https://t.me/RendyProjects",
+                text=_["S_B_2"], callback_data="settings_helper"
             ),
         ],
     ]
@@ -63,22 +50,12 @@ def start_pannel(_):
     return buttons
 
 
-def private_panel(
-    _, 
-    BOT_USERNAME, 
-    START_IMG_URL,
-   OWNER: Union[bool, int] = None
-):
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_8"], callback_data="settings_back_helper"
-            ), 
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/VegetaSupports"
-            ), 
+            )
         ]
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
@@ -90,11 +67,6 @@ def private_panel(
                 InlineKeyboardButton(
                     text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
                 ),
-            ], 
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_9"], url=f"https://t.me/VegetaSupports"
-               ), 
             ]
         )
     else:
@@ -111,12 +83,7 @@ def private_panel(
                 [
                     InlineKeyboardButton(
                         text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_9"], url=f"https://t.me/VegetaSupports"
-                    ), 
+                    )
                 ]
             )
     buttons.append(
