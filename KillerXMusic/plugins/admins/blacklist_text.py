0cb1215f159a3944ea9310476a2462f3cd2f8b62
Cute Inspire <Randywahidin321@gmail.com>
2022-06-22 13:20:00 +0700

    fixed issue soon

---
+++ KillerXMusic/plugins/admins/blacklist_text.py
@@ -0,0 +1,12 @@
+# Credits by @FFmpegNotInstalled
+#from pyrogram import filters
+#from KillerXMusic import app
+
+# blacklist text
+#@app.on_message(filters.text)
+#def delete_text(app, message):
+ #     bot.delete_messages(message.chat.id, message.message_id)
+#     word_list = ["bokep", "vcs", "desah", "porno",] # list random blacklist
+#     if message.text in word_list:
+#        app.delete_messages(message.chat.id, message.message_id)
+#        app.send_message(message.chat.id, "blacklisted blocked")
