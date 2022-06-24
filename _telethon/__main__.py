16fc6655a7b9a64ad3928b391f87fa6caf44dffe
Cute Inspire <Randywahidin321@gmail.com>
2022-06-24 18:27:02 +0700

    Create __main__.py

---
+++ _telethon/__main__.py
@@ -0,0 +1,32 @@
+# (c) code by Randi356 
+# Credits by https://github.com/Randi356/KillerX-Music 
+
+import sys
+import glob
+import logging
+import importlib
+from pathlib import Path
+from telethon import TelegramClient, events
+from _telethon import app, LOGGER
+from _telethon.modules import *
+
+def load_plugins(plugin_name):
+    path = Path(f"_telethon/modules/{plugin_name}.py")
+    name = "_telethon.modules.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["_telethon.modules." + plugin_name] = load
+
+path = "_telethon/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+app.start()
+print("Telethon User Client Started \nPowered By @RendyProjects\n(C) 2022 Rendy")
+app.run_until_disconnected()
